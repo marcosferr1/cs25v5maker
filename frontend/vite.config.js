af4582aea037a988176ada0cbb7029f2725ed6c3
marcosferr1 <marcos.e.ferr@gmail.com>
2025-10-29 16:26:49 -0300

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const base = process.env.VITE_BASE_PATH || '/'
-
 export default defineConfig({
   plugins: [react()],
-  base,
   server: {
     port: 3000,
     host: '0.0.0.0',
